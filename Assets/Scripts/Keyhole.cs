d05ef0fa836f4a386f0c6e6ba299fdfd6481cf35
Alexander Bergman <bergman.alex@gmail.com>
2021-09-28 09:46:25 +0200

    Initial commit

---
+++ Assets/Scripts/Keyhole.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keyhole : MonoBehaviour
+{
+  [SerializeField] private GameObject key = null;
+  [SerializeField] private GameObject door = null;
+
+  private Collider2D keycollider;
+
+  void Start()
+  {
+    keycollider = key.GetComponent<BoxCollider2D>();
+  }
+
+  private void OnTriggerEnter2D(Collider2D collider)
+  {
+    if (collider == keycollider)
+    {
+      door.SetActive(false);
+    }
+  }
+
+  private void OnTriggerExit2D(Collider2D collider)
+  {
+    if (collider == keycollider)
+    {
+      door.SetActive(true);
+    }
+  }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  private CharacterController2D controller;
+  private Rigidbody2D rb;
+  private Animator animator;
+
+  private float moveSpeed = 30f;
+  private float horizontalMovement = 0f;
+  private float verticalMovement = 0f;
+  private bool isJumping = false;
+
+  void Start()
+  {
+    controller = GetComponent<CharacterController2D>();
+    rb = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
+  }
+
+  void Update()
+  {
+    horizontalMovement = rb.velocity.x;
+    verticalMovement = rb.velocity.y;
+
+    if (Input.GetButtonDown("Jump"))
+    {
+      isJumping = true;
+    }
+
+    Animate();
+  }
+
+  void FixedUpdate()
+  {
+    controller.Move(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.fixedDeltaTime, isJumping);
+    isJumping = false;
+  }
+
+  void Animate()
+  {
+    animator.SetFloat("velocityX", rb.velocity.x);
+    animator.SetFloat("velocityY", rb.velocity.y);
+  }
+}
