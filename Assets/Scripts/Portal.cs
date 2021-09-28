f29e39b9db0ff611aa2730b4c25be438e041ff03
bergman-alex <bergman.alex@gmail.com>
2021-09-28 14:35:15 +0200

    added portals

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -6,8 +6,24 @@ public class Portal : MonoBehaviour
 {
   [SerializeField] private GameObject linkedPortal = null;
 
+  [System.NonSerialized] public bool active = true;
+
+
   private void OnTriggerEnter2D(Collider2D collider)
   {
-    collider.gameObject.transform.position = linkedPortal.transform.position;
+    if (!linkedPortal)
+    {
+      Debug.Log("ERROR: No linked portal.");
+    }
+    else if (active)
+    {
+      collider.gameObject.transform.position = linkedPortal.transform.position;
+      linkedPortal.GetComponent<Portal>().active = false;
+    }
+  }
+
+  private void OnTriggerExit2D(Collider2D collider)
+  {
+    active = true;
   }
 }
